a877ace8a521b1c316b9b5e7abfabba1c3b574ae
luisSandino <sandino.ruiz@gmail.com>
2016-03-01 21:23:35 -0600

    changes at css and index

--- JS/controllers.js
+++ JS/controllers.js
@@ -2,37 +2,25 @@
 var myCtrl=angular.module('myAppGame.controllers', [] );
 
 //Controlador de la vista de juegos en parejas solamente
-myCtrl.controller('GameController',['$scope', '$localStorage', function($scope, $localStorage) {
+myCtrl.controller('GameController',['$scope', '$localStorage','$document', function($scope, $localStorage, $document) {
 		//guardo los datos en localStorage
 	$scope.$storage = $localStorage.$default({
       lists: [] 
       
     });
-	
+    
 	$scope.lists = $localStorage.lists;
 //Agrego los nuevos juegos al Array lists
 	$scope.addGame= function() {
 		$scope.lists.push($scope.newGame);
-		$scope.newGame={};
+		$scope.newGame= {};
 	};  
 
 //var plyrName1= document.querySelector('playerName1');
-var plyrName1= angular.element(document.querySelector('#playerName1'));
-var plyrStrtgy1= angular.element(document.querySelector('#playerStrategy1'));
-var plyrName2= angular.element(document.querySelector('#playerName2'));
-var plyrStrtgy2=  angular.element(document.querySelector('#playerStrategy2'));
-
-
-$scope.gameRules = function(plyrName1) {
-	plyr1= plyrName1;
-	if (plyr1==='H') {alert('listo')} else {}
-		return plyr1;
-}
-
-
-
-
-
+//var plyrName1= angular.element(document.querySelector('#playerName1'));
+//var plyrStrtgy1= angular.element(document.querySelector('#playerStrategy1'));
+//var plyrName2= angular.element(document.querySelector('#playerName2'));
+//var plyrStrtgy2=  angular.element(document.querySelector('#playerStrategy2'));
 
 
 
